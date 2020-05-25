d0e83304ce729d1d26cd2cf0da0d389970f6e062
Karbas <karbas@cafebazaar.ir>
2020-05-26 02:31:25 +0430

    Add model and related migrations

--- webapp/campaigns/models.py
+++ webapp/campaigns/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Impression(models.Model):
+    period = models.PositiveSmallIntegerField(db_index=True)
+    banner_id = models.PositiveIntegerField(db_index=True)
+    campaign_id = models.PositiveIntegerField(db_index=True)
+
+    def __str__(self):
+        return (f'Period: {self.period}, '
+                f'BannerId: {self.banner_id}, '
+                f'CampaignId: {self.banner_id}')
+
+
+class Click(models.Model):
+    period = models.PositiveSmallIntegerField(db_index=True)
+    click_id = models.PositiveIntegerField(db_index=True, primary_key=True)
+    banner_id = models.PositiveIntegerField(db_index=True)
+    campaign_id = models.PositiveIntegerField(db_index=True)
+
+    def __str__(self):
+        return (f'Period: {self.period}, '
+                f'ClickId: {self.click_id}, '
+                f'BannerId: {self.banner_id}, '
+                f'CampaignId: {self.banner_id}')
+
+
+class Conversion(models.Model):
+    period = models.PositiveSmallIntegerField(db_index=True)
+    conversion_id = models.PositiveIntegerField(db_index=True, primary_key=True)
+    click_id = models.ForeignKey(Click, on_delete=models.SET_NULL, null=True)
+    revenue = models.FloatField()
+
+    def __str__(self):
+        return (f'Period: {self.period}, '
+                f'ConversionId: {self.conversion_id}, '
+                f'ClickId: {self.click_id}, '
+                f'Revenue: {self.revenue}')
