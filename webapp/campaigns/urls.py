33dac64ce57efe0f866b35f3aaf4b6602524043e
Karbas <karbas@cafebazaar.ir>
2020-05-27 03:19:22 +0430

    Add business logic
    Now the business rules defined in the assignment document are
    implemented.
    
    Caveat: There's no session logic in place yet. Therefore, a client
    may see a certain banner two times in a row.

--- webapp/campaigns/urls.py
+++ webapp/campaigns/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'campaigns'
 urlpatterns = [
-    path('<int:campaign_id>/', views.campaign, name='campaign'),
+    path('<int:campaign_id>/', views.Campaign.as_view(), name='campaign'),
     path('banner/<int:banner_id>/', views.banner, name='banner'),
 ]
--- webapp/campaigns/views.py
+++ webapp/campaigns/views.py
@@ -1,15 +1,62 @@
-from django.shortcuts import render
+import logging
+
+from random import choice as random_choice
+
+from django.db import connection
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils import timezone
+from django.views import View
 
 CAMPAIGN_TEMPLATE_NAME = 'campaigns/campaign.html'
 
+SQL_QUERY_TEMPLATE = '''select imp.banner_id,
+       count(clk.click_id)  as clk_count,
+       SUM(cnv.revenue)     as revenue
+from campaigns_impression as imp
+         LEFT JOIN campaigns_click as clk
+                   on imp.period = clk.period
+                       and imp.banner_id = clk.banner_id
+                       and imp.campaign_id = clk.campaign_id
+         LEFT JOIN campaigns_conversion as cnv
+                   on clk.click_id = cnv.click_id_id
+where imp.period = %s
+  and imp.campaign_id = %s
+GROUP BY imp.banner_id
+ORDER BY revenue desc, clk_count desc
+LIMIT 10;
+'''
 
-def campaign(request, campaign_id):
-    context = {
-        'campaign_id': campaign_id,
-        'banner_id': 102,
-    }
-    return render(request, CAMPAIGN_TEMPLATE_NAME, context)
+logger = logging.getLogger(__name__)
+
+
+class Campaign(View):
+    def get(self, request, campaign_id):
+        period = Campaign.current_period()
+        with connection.cursor() as cursor:
+            cursor.execute(SQL_QUERY_TEMPLATE, [period, campaign_id])
+            rows = cursor.fetchall()
+        banner_id = Campaign.get_banner_id_from_db_result(rows)
+        context = {
+            'campaign_id': campaign_id,
+            'banner_id': banner_id,
+        }
+        return render(request, CAMPAIGN_TEMPLATE_NAME, context)
+
+    @staticmethod
+    def current_period():
+        now_minutes = timezone.localtime().minute
+        return (now_minutes // 15) + 1
+
+    @staticmethod
+    def get_banner_id_from_db_result(result_rows):
+        accepted_banners = []
+        for row in result_rows:
+            if row[2] or len(accepted_banners) < 5:
+                # Banner has revenue or less than 5 banners accepted
+                accepted_banners.append(row[0])
+        logger.debug(f'{accepted_banners=}')
+        return random_choice(accepted_banners)
 
 
 def banner(request, banner_id):
