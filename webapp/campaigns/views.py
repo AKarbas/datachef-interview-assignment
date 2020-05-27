00cf844621d56725102d968370bbc1e6f13b08a6
Karbas <karbas@cafebazaar.ir>
2020-05-27 19:06:17 +0430

    Add session cookies to not show duplicate banners

--- webapp/campaigns/views.py
+++ webapp/campaigns/views.py
@@ -3,13 +3,9 @@ import logging
 from random import choice as random_choice
 
 from django.db import connection
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
 from django.views import View
-
-CAMPAIGN_TEMPLATE_NAME = 'campaigns/campaign.html'
-BANNER_NAME_TEMPLATE = 'image_{}.png'
 
 SQL_QUERY_TEMPLATE = '''select imp.banner_id,
        count(clk.click_id)  as clk_count,
@@ -32,19 +28,25 @@ logger = logging.getLogger(__name__)
 
 
 class Campaign(View):
+    TEMPLATE_NAME = 'campaigns/campaign.html'
+    BANNER_NAME_TEMPLATE = 'image_{}.png'
+    LAST_BANNER_KEY = 'last_banner'
+
     def get(self, request, campaign_id):
         period = Campaign.current_period()
         with connection.cursor() as cursor:
             cursor.execute(SQL_QUERY_TEMPLATE, [period, campaign_id])
             rows = cursor.fetchall()
-        banner_id = Campaign.get_banner_id_from_db_result(rows)
-        banner_name = BANNER_NAME_TEMPLATE.format(banner_id)
+        last_banner = Campaign.get_last_banner(request)
+        banner_id = Campaign.get_banner_id_from_db_result(rows, last_banner)
+        Campaign.set_last_banner(request, banner_id)
+        banner_name = Campaign.BANNER_NAME_TEMPLATE.format(banner_id)
         context = {
             'campaign_id': campaign_id,
             'banner_id': banner_id,
             'banner_name': banner_name,
         }
-        return render(request, CAMPAIGN_TEMPLATE_NAME, context)
+        return render(request, Campaign.TEMPLATE_NAME, context)
 
     @staticmethod
     def current_period():
@@ -52,11 +54,23 @@ class Campaign(View):
         return (now_minutes // 15) + 1
 
     @staticmethod
-    def get_banner_id_from_db_result(result_rows):
+    def get_last_banner(request):
+        return (request.session[Campaign.LAST_BANNER_KEY]
+                if Campaign.LAST_BANNER_KEY in request.session
+                else None)
+
+    @staticmethod
+    def set_last_banner(request, banner_id):
+        request.session[Campaign.LAST_BANNER_KEY] = banner_id
+
+    @staticmethod
+    def get_banner_id_from_db_result(result_rows, last_banner=None):
         accepted_banners = []
         for row in result_rows:
             if row[2] or len(accepted_banners) < 5:
                 # Banner has revenue or less than 5 banners accepted
                 accepted_banners.append(row[0])
+        if last_banner in accepted_banners and len(accepted_banners) > 1:
+            accepted_banners.remove(last_banner)
         logger.debug(f'{accepted_banners=}')
         return random_choice(accepted_banners)
