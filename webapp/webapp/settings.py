9ac77e2f793e2732d43afd6295156fd1b8e15dd4
Karbas <karbas@cafebazaar.ir>
2020-05-29 15:15:06 +0430

    Disable debug mode

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7n2lau6g)m=i^xe_#o_c7r$yx1z^aycax2=xx@zq(&9wswxa9@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
